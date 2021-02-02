5069beebdc787c8a9a1e0a2f72914dbb1fcf6b99
eldanmo <70357119+eldanmo@users.noreply.github.com>
2021-02-02 15:27:58 -0500

    hdv 1
    Hoja de vida Daniel Mora

---
+++ js/styles.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar',{
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+    })
+
+    sr.reveal('.ei',{
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+    })
+
+    sr.reveal('.ed',{
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px',
+    })
+
+    function cambiarTexto() {
+    document.getElementById("div1").innerHTML="<p>okokokookokkok</p>";
+    }
+
+    
